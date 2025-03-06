ca799cc3634e4afaf5187bd04c30ca3360c7655a
Randy Wayberg <rwayberg@gmail.com>
2025-03-06 14:52:48 -0500

    CRUD functions for Items

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
-from fastapi import FastAPI, status, Response
+from fastapi import FastAPI, status, Response, HTTPException
 from typing import Union, Any
 from pydantic import BaseModel
 from pythonAPI.models.item_model import Item # type: ignore
 from pythonAPI.models.item_model import test_items # type: ignore
+from pythonAPI.models.item_model import ItemUpdate # type: ignore
 
 app = FastAPI()
-
-
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+def read_item(item_id: int) -> Item:
+   try:
+        return next(x for x in test_items if x.id == item_id)
+   except StopIteration:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No item found")
 
 @app.get("/items/")
 def read_all_items() -> list[Item]:
@@ -22,6 +24,43 @@ def read_all_items() -> list[Item]:
 
 
 @app.post("/items/", status_code=status.HTTP_201_CREATED)
-def create_item(item: Item) -> Any:
+def create_item(item: Item):
+    if item.id <= 0:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Item id must be greater than zero")
+    if any(x.id == item.id for x in test_items):
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Item with that id already exists")
     test_items.append(item)
     return {"message":"Item accepted"}
+
+@app.patch("/items/", status_code=status.HTTP_200_OK)
+def accept_item(item_id: int):
+    try:
+        found_item = next(x for x in test_items if x.id == item_id)
+        found_item.accepted = True
+        return {"message": "Item updated to accepted"}
+    except StopIteration:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No item found")
+    
+@app.post("/items", status_code=status.HTTP_202_ACCEPTED)
+def update_item(update_item: ItemUpdate) -> Item:
+    try:
+        found_item = next(x for x in test_items if x.id == update_item.id)
+        if update_item.name is not None:
+            found_item.name = update_item.name
+        if update_item.description is not None:
+            found_item.description = update_item.description
+        if update_item.price is not None:
+            found_item.price = update_item.price
+        return found_item
+    except StopIteration:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No item found")
+    
+@app.delete("/items/", status_code=status.HTTP_200_OK)
+def delete_item(item_id: int):
+    try:
+        found_item = next(x for x in test_items if x.id == item_id)
+        test_items.remove(found_item)
+        return {"message":"Item was removed"}
+    except StopIteration:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No item found")
+    
--- models/item_model.py
+++ models/item_model.py
@@ -1,19 +1,26 @@
 from pydantic import BaseModel, Field
 
 class Item(BaseModel):
-    _id: int
+    id: int
     name: str
     description: str | None = None
     price: float
     accepted: bool | None = False
-    @property
-    def ID(self) -> int:
-        return self._id
+
+class ItemUpdate(BaseModel):
+    id: int
+    name: str | None = None
+    description: str | None = None
+    price: float | None = None
+
+    #@property
+    #def ID(self) -> int:
+    #    return self._id
     
 
 # Testing Data
-test_item_1 = Item(name="Test item 1", description="Test description 1", price=0.23, accepted=False)
-test_item_2 = Item(name="Test item 2", description="Test description 2", price=35.67, accepted=False)
-test_item_3 = Item(name="Test item 3", description="Test description 3", price=546.00, accepted=True)
+test_item_1 = Item(id=1, name="Test item 1", description="Test description 1", price=0.23, accepted=False)
+test_item_2 = Item(id=2, name="Test item 2", description="Test description 2", price=35.67, accepted=False)
+test_item_3 = Item(id=3, name="Test item 3", description="Test description 3", price=546.00, accepted=True)
 
 test_items = [test_item_1, test_item_2, test_item_3]
